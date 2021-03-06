c5cc8326def0fae4a62dbd615871805d9e044579
Leon Starr <leon_starr@modelint.com>
2021-03-06 11:27:22 -0800

    Improved the documentation in the frame module. Also cleaned up some working, but awkard code. Also spotted a few failure scenarios not triggered by the current example and pre-emptively fixed them. Finally, added more logging

--- flatland/sheet_subsystem/frame.py
+++ flatland/sheet_subsystem/frame.py
@@ -21,21 +21,29 @@ FieldPlacement = namedtuple('_FieldPlacement', 'metadata position max_area')
 
 class Frame:
     """
-    On any serious project it is not adequate to generate model diagrams absent any meta data such as
+    On any serious project it is not adequate to generate model diagrams absent any metadata such as
     authors, dates, revision numbers, copyright notices, organization logos and so forth.
     A Frame represents a pattern of Fields and/or a Title Block Pattern on the surface area defined by
     a Sheet. The lower left corner placements of each Frame element (Field or Scaled Title Block) are
     customized to fit the dimensions of a given Sheet.
+
+        Attributes
+
+        - Name (str) -- Size independent name of the Frame such as 'Open Source Engineer' or 'Architect'
+        - Canvas (obj) -- Canvas has selected a Sheet which determines Frame sizing
+        - metadata (dict) -- <Metadata> : <Content>, such as 'Title' : 'Sheet Subsystem Class Diagram'
+        - Open_fields (list) -- Open field metadata label and positional info loaded from flatland database
+        - Databoxes (dict) -- All Databox data loaded from flatland database (See named tuple above)
     """
 
     def __init__(self, name: str, presentation: str, canvas: 'Canvas', metadata: Dict[str, str]):
         """
         Constructor
 
-        :param name:
-        :param canvas:
-        :param presentation:
-        :param metadata:
+        :param name: Size independent name of the Frame such as 'Open Source Engineer' or 'Architect'
+        :param canvas: Canvas has selected the Sheet which determines sizing
+        :param presentation: They Frame's Presentation (determines all styling of Frame content)
+        :param metadata: Text and images to display in the Frame
         """
         self.logger = logging.getLogger(__name__)
         self.Name = name
@@ -43,14 +51,20 @@ class Frame:
         self.metadata = metadata
         self.Open_fields = []
         self.Databoxes = {}
-        self.Box_fields = []
 
-        # The Frame's drawing type name is composed from the frame's name and size
+        # Create a Layer where we'll draw all of the Frame contents
+
+        self.logger.info('Creating Frame Layer')
         drawing_type_name = ' '.join([name, self.Canvas.Sheet.Size_group])  # e.g. "OS Engineer large"
-        # Now create a Layer for the Frame
+        # Whereas a diagram's drawing type is something like 'xUML Class Diagram',
+        # the Frame's drawing type name systematically incorporates both purpose and Sheet Size Group
+        # That's because a model element like a class or state is typically drawn the same size regardless
+        # of sheet size. Frame's, on the other hand are more likely to change proportions with large sheet size
+        # differences.  That said, there is nothing preventing us from doing the same for diagram layers on a case by
+        # case basis. So an 'xUML Class Diagram tiny' could certainly be defined by us or a user in the future
         self.Layer = self.Canvas.Tablet.add_layer(
             name='frame', presentation=presentation, drawing_type=drawing_type_name
-        )  # Now we have a surface to draw the frame on!
+        )  # We're gonna be drawing metadata and title block borders all over this thing.
 
         # If there is a title block placement specified for this Frame, get the name of the pattern
         tb_placement_t = fdb.MetaData.tables['Title Block Placement']
@@ -60,16 +74,22 @@ class Frame:
         )
         query = select([tb_placement_t.c['Title block pattern']]).select_from(tb_placement_t).where(f)
         row = fdb.Connection.execute(query).fetchone()
+        # Nothing says "I'm a serious engineer or architect" more than a fancy bordered title block on your Frame,
+        # but it is optional
         self.Title_block_pattern = None if not row else row[0]
 
+        # If a Title Block Pattern is specified, let's gather all the Data Box content from the flatland database
         if self.Title_block_pattern:
-            # Build a text block for each Data Box containing the Metadata Text Content
-            # Resource Metacontent (graphics) is not allowed like it is for Open Fields, so
-            # we assume all Metacontent is text
-            boxplace_t = fdb.MetaData.tables['Box Placement']
-            databox_t = fdb.MetaData.tables['Data Box']
-            boxline_t = fdb.MetaData.tables['Box Text Line']
-            f = and_(
+            self.logger.info('Assembling title block pattern on frame')
+            # Assemble a text block for each Data Box containing the Metadata Text Content
+            # We'll register that text block with the Layer for rendering
+            # Image (Resource) content is not supported within a Title Block Pattern, so we assume only text content
+            # If any non-text Resources were mistakenly specified by the user, we will ignore them
+            boxplace_t = fdb.MetaData.tables['Box Placement']  # Box positions and sizes, scaled for our Frame
+            databox_t = fdb.MetaData.tables['Data Box']  # Alignment and style of text within box
+            boxline_t = fdb.MetaData.tables['Box Text Line']  # Vertical ordering of metadata within a Data Box
+            # s = select (row criteria), p = project (columns), j = join, q = query
+            s = and_(
                 (boxplace_t.c.Frame == self.Name),
                 (boxplace_t.c.Sheet == self.Canvas.Sheet.Name),
                 (boxplace_t.c['Title block pattern'] == self.Title_block_pattern),
@@ -84,11 +104,9 @@ class Frame:
                 boxline_t,
                 and_((databox_t.c.Pattern == boxline_t.c['Title block pattern']), (databox_t.c.ID == boxline_t.c.Box))
             )
-            q = select(p).select_from(j).where(f).order_by(boxplace_t.c.Box, boxline_t.c.Order)
-            # q = select(p).select_from(j).where(f)
+            q = select(p).select_from(j).where(s).order_by(boxplace_t.c.Box, boxline_t.c.Order)
             rows = fdb.Connection.execute(q).fetchall()
-            # Lookup the Text Content for each Box Text Line and create
-            # a text block for each Data Box
+            # Populate our Databoxes dictionary from the row data we just fetched
             for r in rows:
                 if r.Box in self.Databoxes:
                     # The Data Box was recorded with an initial text line, so this must be an additional line
@@ -97,28 +115,34 @@ class Frame:
                     # Rows are ordered by Data Box, so if the box id is new, we create an initial dictioary entry
                     # With level 1
                     self.Databoxes[r.Box] = DataBox(
-                        content=[metadata[r.Metadata][0]],
-                        position=Position(r.X, r.Y),
+                        content=[metadata[r.Metadata][0]],  # Text to render:  Leon Starr, mint.flatland.td.1, etc
+                        position=Position(r.X, r.Y),  # Lower left corner of the Data Box
                         size=Rect_Size(height=r.Height, width=r.Width),
-                        style=r.Style, metadata=r.Metadata,
+                        style=r.Style,  # Style of text inside this box such as: Block body, Block title, etc
+                        metadata=r.Metadata,  # Name of data item: Author, Document ID, etc
+                        # Finally, how text is aligned inside this box
                         alignment=Alignment(vertical=VertAlign[r['V align']], horizontal=HorizAlign[r['H align']])
                     )
+            # All done with the title block Metacontent. We'll unwind all this when we render to our Layer
 
-        # Render the non-title block fields
+        # Gather the Open Field content (other text and graphics scattered around the Frame)
+        self.logger.info('Assembling open fields on frame')
         open_field_t = fdb.MetaData.tables['Open Field']
-
-        f = and_(
+        s = and_(
             (open_field_t.c['Frame'] == self.Name),
             (open_field_t.c['Sheet'] == self.Canvas.Sheet.Name)
         )
-        q = select([open_field_t]).where(f)
+        q = select([open_field_t]).where(s)
         rows = fdb.Connection.execute(q).fetchall()
         for r in rows:
-            p = Position(r['x position'] * points_in_mm, r['y position'] * points_in_mm)
-            ma = Rect_Size(r['max height'] * points_in_mm, r['max width'] * points_in_mm)
+            p = Position(round(r['x position'] * points_in_mm, 2), round(r['y position'] * points_in_mm, 2))
+            ma = Rect_Size(round(r['max height'] * points_in_mm, 2), round(r['max width'] * points_in_mm, 2))
             self.Open_fields.append(
                 FieldPlacement(metadata=r.Metadata, position=p, max_area=ma)
             )
+        # All done with the Open Fields. Much easier to compute than Boxed Fields!
+
+        # Now let's register all text and graphics for everything in our Frame on its Layer
         self.render()
 
     def render(self):
@@ -127,7 +151,7 @@ class Frame:
 
         # Fill each open field
         for f in self.Open_fields:
-            a = ' '.join(['Open', f.metadata.lower(), self.Canvas.Sheet.Size_group])
+            asset = ' '.join(['Open', f.metadata.lower(), self.Canvas.Sheet.Size_group])
             content, isresource = self.metadata.get(f.metadata, (None, None))
             # If there is no data supplied to fill in the field, just leave it blank and move on
             if content and isresource:
@@ -140,42 +164,51 @@ class Frame:
             elif content:  # Text content
                 # Content is a line of text to print directly
                 self.Layer.add_text_line(
-                    asset=a,
+                    asset=asset,
                     lower_left=f.position,
                     text=content,
                 )
 
-        # Draw the title block, if any
-        draw_titleblock(frame=self.Name, sheet=self.Canvas.Sheet, layer=self.Layer)
+        if self.Title_block_pattern:
+            # Draw the title block box borders
+            draw_titleblock(frame=self.Name, sheet=self.Canvas.Sheet, layer=self.Layer)
 
-        # Get the margin to use in each Data Box
-        tb_place_t = fdb.MetaData.tables['Title Block Placement']
-        scaledtb_t = fdb.MetaData.tables['Scaled Title Block']
-        p = [scaledtb_t.c['Margin H'], scaledtb_t.c['Margin V']]
-        j = tb_place_t.join(scaledtb_t)
-        q = select(p).select_from(j).where(tb_place_t.c.Frame == self.Name)
-        row = fdb.Connection.execute(q).fetchone()
-        assert row, f"No Title Block Placement for frame: {name}"
-        h_margin, v_margin = row
-
-        # Render all the box fields
-        for k, v in self.Databoxes.items():
-            # compute lower left corner position
-            # Layer asset is composed from the data box style and its size group
-            asset = ' '.join([v.style, self.Canvas.Sheet.Size_group])
-            block_size = self.Layer.text_block_size(asset=asset, text_block=v.content)
-            # When there is a single line of text in a Data Box that is longer than the Box width,
-            # we will wrap it as necessary. Especially useful for a long title in the title box
-            # For multiple line boxes, this feature is not yet (or ever) supported
-            padded_box_width = round(v.size.width - h_margin * 2, 2)
-            content = v.content
-            if len(v.content) == 1 and block_size.width > padded_box_width:
-                lines_to_wrap = math.ceil(block_size.width / padded_box_width)  # Round up to nearest int
-                content = TextBlock(v.content[0], wrap=lines_to_wrap).text
-                block_size = self.Layer.text_block_size(asset=asset, text_block=content)
-
-            xpos = v.position.x + h_margin
-            ypos = v.position.y + v_margin + round((v.size.height - block_size.height) / 2, 2)
-            self.Layer.add_text_block(
-                asset=asset, lower_left=Position(xpos, ypos), text=content, align=v.alignment.horizontal
+            # Get the margins to pad the Data Box content
+            # The same margins are applied to each Data Box in the same Scaled Title Block
+            # So we are looking only for one pair of h,v margin values to use throughout
+            scaledtb_t = fdb.MetaData.tables['Scaled Title Block']
+            s = and_(
+                (scaledtb_t.c['Title block pattern'] == self.Title_block_pattern),
+                (scaledtb_t.c['Sheet size group'] == self.Canvas.Sheet.Size_group),
             )
+            p = [scaledtb_t.c['Margin H'], scaledtb_t.c['Margin V']]
+            q = select(p).where(s)
+            row = fdb.Connection.execute(q).fetchone()
+            assert row, f"No Title Block Placement for frame: {name}"
+            h_margin, v_margin = row
+
+            # Render all the box fields
+            for k, v in self.Databoxes.items():
+                # compute lower left corner position
+                # Layer asset is composed from the data box style and its size group
+                asset = ' '.join([v.style, self.Canvas.Sheet.Size_group])
+                block_size = self.Layer.text_block_size(asset=asset, text_block=v.content)
+                # When there is a single line of text in a Data Box that is longer than the Box width,
+                # we will wrap it as necessary. Especially useful for a long title in the title box
+                # For multiple line boxes, this feature is not yet (or ever) supported
+                padded_box_width = round(v.size.width - h_margin * 2, 2)
+                content = v.content
+                if len(v.content) == 1 and block_size.width > padded_box_width:
+                    lines_to_wrap = math.ceil(block_size.width / padded_box_width)  # Round up to nearest int
+                    content = TextBlock(v.content[0], wrap=lines_to_wrap).text
+                    block_size = self.Layer.text_block_size(asset=asset, text_block=content)
+                # For now we will just let any exessive line length in multi field Data Boxes
+                # overrun the title block boundary, since it is not so obvious how to recover automatically
+                # User correction is more likely to yield a satisfactory outcome
+                # They can either use more box fields, use shorter lines of text, change the size of
+                # the Title Block Pattern or just create one that is larger and use that
+                xpos = v.position.x + h_margin
+                ypos = v.position.y + v_margin + round((v.size.height - block_size.height) / 2, 2)
+                self.Layer.add_text_block(
+                    asset=asset, lower_left=Position(xpos, ypos), text=content, align=v.alignment.horizontal
+                )
