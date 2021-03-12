b73ddede05cbaa39a8022a126f0cff98817ffe8e
Leon Starr <leon_starr@modelint.com>
2021-03-12 14:38:06 -0800

    Added resource locator for toyota logo.  (The resource locator will be moved out to a YAML file soon, but this works for now)

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ README = (HERE / "README.md").read_text()
 
 setuptools.setup(
     name="flatland-model-diagram-editor",
-    version="0.16.1",
+    version="0.16.2",
     description="Model text file + layout text file -> beautiful diagram",
     long_description=README,
     long_description_content_type="text/markdown",
